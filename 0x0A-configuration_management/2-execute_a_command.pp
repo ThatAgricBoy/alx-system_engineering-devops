a82b6a4bb555dc4e815b5b257895055efd147716
ThatAgricBoy <samueljohn3999@gmail.com>
2023-06-23 18:10:01 +0100

    kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # How to use the exec resource of Puppet
 
 exec { 'kill_process':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
- }
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+}
