2c67ceacb6063a2c13bae2eb75860ba7fe8c3e33
ThatAgricBoy <samueljohn3999@gmail.com>
2023-06-24 20:37:38 +0100

    script to configure server

---
+++ 0x0B-ssh/let_me_in.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Server details
+server=35.153.98.251
+user="ubuntu"
+
+# SSH public key
+ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
+
+# Connect to the server and add the SSH public key to authorized_keys
+ssh ubuntu@35.153.98.251 "mkdir -p ~/.ssh && echo \"$ssh_public_key\" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
+
+echo "SSH public key added to the server for user '$user'."
