a285a33696abf5a3d2acaa42d54b32211726b6a7
Samuel John <samueljohn3999@gmail.com>
2023-07-25 10:23:32 +0100

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """ Python to get data from an API and convert to Json"""
-import csv
 import json
 import requests
 import sys
 
 
-if __name__ == '__main__':
-    USER_ID = sys.argv[1]
-    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+def get_user_data(user_id):
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + str(user_id)
     res = requests.get(url_to_user)
-    """Documentation"""
-    USER_DATA = res.json()
-    USERNAME = USER_DATA.get('username')
-    """Documentation"""
+    user_data = res.json()
+
     url_to_task = url_to_user + '/todos'
     res = requests.get(url_to_task)
     tasks = res.json()
@@ -25,10 +21,15 @@ if __name__ == '__main__':
         task_list.append({
             "task": TASK_TITLE,
             "completed": TASK_COMPLETED_STATUS,
-            "username": USERNAME
+            "username": user_data.get('username')
         })
 
-    dict_data = {USER_ID: task_list}
+    return task_list
+
+
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    dict_data = {USER_ID: get_user_data(USER_ID)}
 
     with open('{}.json'.format(USER_ID), 'w') as f:
         json.dump(dict_data, f, indent=4)
