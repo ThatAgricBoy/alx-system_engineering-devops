61bbb37aa4b8e57a4f1e89d8e1a3f7371fdea0fd
ThatAgricBoy <samueljohn3999@gmail.com>
2023-08-08 15:35:54 +0100

    getting subreddits

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+"""Write a function that queries the Reddit API and
+   returns the number of subscribers"""
+
+
+def number_of_subscribers(subreddit):
+    """A function that returns the number of subscribers for a
+    given subreddit"""
+    user_agent = "0-subs/1.0"
+    headers = {"User-Agent": user_agent}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    elif response.status_code == 404:
+        return 0
+    else:
+        print(f"Error: {response.status_code}")
