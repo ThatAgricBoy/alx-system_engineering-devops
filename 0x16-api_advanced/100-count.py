1dbbfdf31a137eaf6b321eb61969f04254e0d706
ThatAgricBoy <samueljohn3999@gmail.com>
2023-08-08 16:40:31 +0100

    count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Reddits"""
+import requests
+from collections import Counter
+
+
+def count_words(subreddit, word_list, after=None):
+    """Recursive"""
+    user_agent = "MyRedditBot/1.0"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"after": after} if after else {}
+    headers = {"User-Agent": user_agent}    
+    response = requests.get(url, headers=headers, params=params)    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        titles = [post["data"]["title"].lower() for post in posts]
+        keywords = [word.lower() for word in word_list]
+        keyword_counter = Counter()
+        for title in titles:
+            for keyword in keywords:
+                if f' {keyword} ' in f' {title} ':
+                    keyword_counter[keyword] += 1
+        if "after" in data["data"] and data["data"]["after"] is not None:
+            return count_words(subreddit, word_list, after=data["data"]["after"])
+        else:
+            sorted_keywords = sorted(keyword_counter.items(),
+                                     key=lambda item: (-item[1], item[0]))
+            for keyword, count in sorted_keywords:
+                print(f"{keyword}: {count}")
+    elif response.status_code == 404:
+        return None
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+subreddit_name = "python"
+keywords = ["python", "java", "programming"]
+count_words(subreddit_name, keywords)
